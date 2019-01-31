3d046e06b3d7a88f9b8c7c2377887cfa51928c78
Salah Eddine ElOuali <Salah Eddine ElOuali@192.168.1.8>
2019-01-31 23:30:56 +0100

    Setting it for Rachid

--- src/main/java/com/ProthAppApplication.java
+++ src/main/java/com/ProthAppApplication.java
@@ -3,11 +3,16 @@ package com;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import com.entities.Patient;
+import com.entities.Therapeute;
 import com.enumeration.GrpSanguin;
 import com.enumeration.Sexe;
+import com.repository.PatientRepository;
+import com.repository.TherapeuteRepository;
 import com.repository.UserRepository;
 
 @SpringBootApplication
@@ -17,27 +22,47 @@ public class ProthAppApplication {
 	UserRepository userRepository;
 	
 	public static void main(String[] args) {
-	Patient p = new Patient();
-	p.setUsername("elouali");
-	p.setPassword("456");
-	p.setNom("ELOUALI");
-	p.setPrenom("SALAH");
-	p.setAdresse("MARACKECH");
-	p.setTelephone("090909090");
-	p.setAccident("TrainAccident");
-	p.setCreatedAt(new Date());
-	p.setDate_accident(new Date());
-	p.setGrp_sanguin(GrpSanguin.A);
-	p.setPoids(122);
-	p.setTaille(189);
-	p.setDate_naissance(new Date());
-	p.setCreatedAt(new Date());
-	p.setSexe(Sexe.MALE);
+		ApplicationContext context=	SpringApplication.run(ProthAppApplication.class, args);
+		
+		PatientRepository up =context.getBean(PatientRepository.class);
+		Patient p = new Patient();
+		
+		p.setId((long) 1);
+		p.setUsername("root");
+		p.setPassword("root");
+		p.setNom("KADAOUI");
+		p.setPrenom("Rachid");
+		p.setAdresse("MARACKECH");
+		p.setTelephone("090909090");
+		p.setAccident("TrainAccident");
+		p.setCreatedAt(new Date());
+		p.setDate_accident(new Date());
+		p.setGrp_sanguin(GrpSanguin.A);
+		p.setPoids(122);
+		p.setTaille(189);
+		p.setDate_naissance(new Date());
+		p.setCreatedAt(new Date());
+		p.setSexe(Sexe.MALE);
 
-	//pu.save(p);
+		up.save(p);
+		
+		TherapeuteRepository ut =context.getBean(TherapeuteRepository.class);
+		Therapeute t = new Therapeute();
+		
+		t.setId((long) 7);
+		t.setUsername("root");
+		t.setPassword("root");
+		t.setNom("KADAOUI");
+		t.setPrenom("Rachid");
+		t.setAdresse("MARACKECH");
+		t.setTelephone("090909090");
+		t.setCreatedAt(new Date());
+		t.setCreatedAt(new Date());
+		
+		ut.save(t);
+		
+		
 	}
-	
-	
 	
 	
 }
